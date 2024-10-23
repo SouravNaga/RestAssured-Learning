ac0387f16e516eecb153f838a162fd7704a2659c
Sourav Naga <souravnaga97@gmail.com>
2024-10-23 08:17:47 +0530

    First setup added

---
+++ src/test/java/org/example/FirstTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    void setup()
+    {
+        System.out.println("test function");
+    }
+}
